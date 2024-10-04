5aeec76b1e4317685978a44936eb1175ee380ca9
Arpit Patidar <44097148+iamarpitpatidar@users.noreply.github.com>
2024-10-04 16:31:04 +0530

    Update vm.php

--- config/vm.php
+++ config/vm.php
@@ -4,7 +4,7 @@ return [
     'api_key' => env('DIGITALOCEAN_API_KEY'),
     'region' => 'blr1',
     'size' => 's-2vcpu-4gb-120gb-intel',
-    'snapshot' => '166945669',
+    'snapshot' => '166959132',
     'tags' => ['expolt_vm'],
     'ssh_keys' => []
 ];
